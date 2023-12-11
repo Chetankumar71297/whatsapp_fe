7eb98223311c75fc523c3e43a7e363f24e5da031
Chetan kumar <chetankumar71297@gmail.com>
2023-12-12 00:04:31 +0530

    Add tailwind and do initial tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import { ChatIcon, ArrowIcon } from "./svg";
-
+import { CallIcon } from "./svg";
 function App() {
   return (
-    <div>
-      <h1>welcome to the frontend</h1>
-      <ChatIcon className="red" />
-      <ArrowIcon />
+    <div className="dark">
+      <h1 className="dark:bg-dark_bg_1">
+        <p className="text-black dark:text-white">
+          styled from variables in tailwind.config file
+        </p>
+      </h1>
+      <h1 className="customh1">i am already styled in index.css</h1>
     </div>
   );
 }
