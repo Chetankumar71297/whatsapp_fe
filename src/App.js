1945b5986702b0f07f81f8ff0c437a26ed1cd772
Chetan kumar <chetankumar71297@gmail.com>
2023-12-12 19:59:01 +0530

    Add redux toolkit and redux persist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Pages/home";
-import Login from "./Pages/login";
-import Register from "./Pages/register";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import { useDispatch } from "react-redux";
+import { logout } from "./features/userSlice";
 
 function App() {
+  const dispatch = useDispatch(logout());
   return (
     <div className="dark">
+      <button onClick={() => dispatch(logout())}>logout</button>
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
