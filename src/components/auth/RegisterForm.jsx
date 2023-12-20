002eb46abed9863b10ce9f67ea7d538419f4042e
Chetan kumar <chetankumar71297@gmail.com>
2023-12-20 17:05:50 +0530

    Setup react-hook-form with form validation with yup also move LoginForm and RegisterForm in auth folder

---
+++ src/components/auth/RegisterForm.jsx
@@ -0,0 +1,59 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { signUpSchema } from "../../utils/validation";
+import AuthInput from "./AuthInput";
+
+export default function RegisterForm() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(signUpSchema) });
+
+  const onSubmit = (data) => console.log(data);
+  return (
+    <div className="h-screen w-full flex items-center justify-center overflow-hidden">
+      {/*container*/}
+      <div className="max-w-md space-y-8 p-10 dark:bg-dark_bg_2 rounded-xl">
+        {/*heading*/}
+        <div className="text-center dark:text-dark_text_1">
+          <h2 className="mt-2 text-3xl font-bold">Welcome</h2>
+          <p className="mt-2 text-sm">Sign up</p>
+          {/*Form*/}
+          <form onSubmit={handleSubmit(onSubmit)} className="mt-6 space-y-6">
+            <AuthInput
+              name="name"
+              type="text"
+              placeholder="Full name"
+              register={register}
+              error={errors?.name?.message}
+            />
+            <AuthInput
+              name="email"
+              type="text"
+              placeholder="Email address"
+              register={register}
+              error={errors?.email?.message}
+            />
+            <AuthInput
+              name="status"
+              type="text"
+              placeholder="Status"
+              register={register}
+              error={errors?.status?.message}
+            />
+            <AuthInput
+              name="password"
+              type="password"
+              placeholder="Password"
+              register={register}
+              error={errors?.password?.message}
+            />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/utils/validation.js
@@ -0,0 +1,22 @@
+import * as Yup from "yup";
+
+export const signUpSchema = Yup.object({
+  name: Yup.string()
+    .required("Name is required")
+    .matches(/^[a-zA-Z_ ]+$/, "Only letters,underscore and spaces are allowed.")
+    .min(2, "Name must be 2 to 16 characters long.")
+    .max(16, "Name must be 2 to 16 characters long."),
+  email: Yup.string()
+    .required("Email address is required.")
+    .email("Invalid email address."),
+  status: Yup.string().max(
+    64,
+    "Status should not contain more than 64 characters."
+  ),
+  password: Yup.string()
+    .required("Password is required.")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
+      "Password must be atleast 6 characters long and should contain atleast 1 uppercase, 1 lowercase, 1 number and 1 special character."
+    ),
+});
